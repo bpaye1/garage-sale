ada27d99b6927cbf0dab568d04adc7456f3a9181
ben.payette@yahoo.com <ben.payette@yahoo.com>
2017-04-10 20:43:45 -0500

    Added React and react-hot-loader

--- app/index.js
+++ app/index.js
@@ -1,6 +1,19 @@
-function component() {
-    let element = document.createElement("div");
-    element.innerHTML = "Hello World wehhoosos";
-    return element;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './components/app';
+
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('react-root')
+    )
+};
+
+render(App);
+
+if (module.hot) {
+    module.hot.accept(App, () => { render(App) })
 }
-document.body.appendChild(component());
--- webpack.babel.config.js
+++ webpack.babel.config.js
@@ -1,19 +1,28 @@
 import path from 'path';
+import { HotModuleReplacementPlugin, NoEmitOnErrorsPlugin } from 'webpack';
 
 export default {
-    entry: ['./app/index', 'webpack-hot-middleware/client'],
+    entry: ['react-hot-loader/patch', './app/index', 'webpack-hot-middleware/client'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'js/bundle.js',
         publicPath: '/'
     },
+    plugins:[
+        new HotModuleReplacementPlugin(),
+        new NoEmitOnErrorsPlugin()
+    ],
     module: {
         rules: [
             {
-                test: /\.s?$/, 
-                exclude: /node_modules/, 
-                use: 'babel=loader'
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'app'),
+                loader: 'babel-loader'
             }
         ]
-    }
+    },
+    resolve: {
+        extensions: ['.js']
+    },
 }
